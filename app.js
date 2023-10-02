ec69a4bd172d6c9299e78aa970e4b1988be62b0e
zamarz <zachmarzouk@gmail.com>
2023-10-02 12:16:55 +0100

    "Add scroll up"

---
+++ app.js
@@ -0,0 +1,24 @@
+const scrollUp = document.getElementById("scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
